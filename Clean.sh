94bad56d96bd31a1f7d3e53b6234a7fd18c6441f
Modrogeanu Cosmin <xeonangel@andromeda.localdomain>
2019-01-08 22:14:40 +0200

    Clean Script Added

---
+++ Clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm /home/xeonangel/Desktop/Tema:Flex_Bison/Hm.lex.c
+rm /home/xeonangel/Desktop/Tema:Flex_Bison/Hm.tab.c
+rm /home/xeonangel/Desktop/Tema:Flex_Bison/Hm.tab.h
+rm /home/xeonangel/Desktop/Tema:Flex_Bison/Hm.out
+
